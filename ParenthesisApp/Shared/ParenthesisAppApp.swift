588af82e260c32dc8709333785747db9e9ff70f6
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2021-01-23 15:19:05 +0900

    ParenthesisApp

---
+++ ParenthesisApp/Shared/ParenthesisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParenthesisAppApp.swift
+//  Shared
+//
+//  Created by Guillermo Ignacio Enriquez Gutierrez on 2021/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ParenthesisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
